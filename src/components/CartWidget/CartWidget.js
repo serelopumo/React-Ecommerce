c029d52e65579a21c5d3c87b06c1f81f84c5ec39
Sere Lopumo <serelopumo97@gmail.com>
2022-07-17 21:55:32 -0300

    Agrego components

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,13 @@
+import './CartWidget.css';
+
+
+const CartWidget = () => {
+<script src="https://kit.fontawesome.com/aa72380eaf.js" crossorigin="anonymous"></script>
+    return(
+        <div className="cartWidget">
+            <img className="cartWidget--image" src='images/shopping-cart.png' alt="card-widget"/>
+            <h4 className="cartWidget--text">1</h4>
+        </div>
+    )
+}
+export default CartWidget;
---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,13 @@
+import './ItemListContainer.css';
+
+const ItemListContainer = () => {
+    return(
+        <div className="ItemListContainer">
+            <h2>Disfrutá del Afternoon Tea con nuestros Blends Orgánicos.<br/>
+            Compartí un momento
+            </h2>
+        </div>
+    )
+}
+
+export default ItemListContainer;
